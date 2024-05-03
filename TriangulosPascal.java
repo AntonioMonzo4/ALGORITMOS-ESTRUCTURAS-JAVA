61aba8f80cf8213dd03d6579f22b39b70ca3b6b9
AntonioMonzo4 <amonansu@gmail.com>
2024-05-03 13:28:23 +0200

    Algoritmo 5-TrianguloPascal

--- TriangulosPascal.java
+++ TriangulosPascal.java
@@ -1,19 +1,20 @@
 public class TriangulosPascal {
-    
-    public static int[][] pascal(int n){
 
-        int [][]array=new int[n][n];
+    public static int[][] pascal(int n) {
 
-        for(int i=0; i<n;i++){
+        int[][] array = new int[n][n];
 
-            for(int j=0; j<=i;j++){
-                if (i==j||i==0) {
-                    array[i][j]=1;
-                }else{
-                    array[i][j]=array[i-1][j-1]+array[i-1][j];
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                if (i == j || i == 0) {
+                    array[i][j] = 1;
+                } else {
+                    array[i][j] = array[i - 1][j - 1] + array[i - 1][j];
                 }
             }
         }
+        return array;
     }
 
 }
